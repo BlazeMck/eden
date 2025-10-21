42da32d36f534a298496208a510067cc73cbe2a2
Riley McKinlay <blazemckinlay@gmail.com>
2025-10-21 10:53:33 -0600

    Start the checkout page and accessing the needed variables to define and fill the page.

--- pages/checkout.php
+++ pages/checkout.php
@@ -4,6 +4,23 @@
 ?>
 </head>
 <body>
+	<h1>Checkout</h1>
+	<div>
+	<?php 
+		$cart = null;
+		if (isset($_SESSION['cart'])) {
+			$cart = $_SESSION['cart'];
+		} elseif (isset($_COOKIE['cart'])) {
+			$cart = json_decode($_COOKIE['cart']);
+		}
+		
+		if ($cart) {
+
+		} else {
+			echo '<h3>Cart is currently empty.</h3>';
+		}
+	?>
+	</div>
 <?php
 	include('../includes/footer.html');
 ?>
