844548e9fc0df4363caac95ccf1fe6b35d45e7cf
Riley McKinlay <blazemckinlay@gmail.com>
2025-10-21 14:24:28 -0600

    Start the basis for implementing stripe.

--- pages/checkout.php
+++ pages/checkout.php
@@ -1,9 +1,22 @@
 <?php
 	$page_title = 'Checkout';
 	include('../includes/header.html');
+    require_once('../util/keys.php');
+    $stripe = new \Stripe\StripeClient($S_SECRET);
 ?>
 </head>
 <body>
+    <h1>Checkout</h1>
+    <?php
+        $session = $stripe->checkout->sessions->create([
+            'line_items' => [
+            [
+                'price' => '15.99',
+                'quantity' => 1,
+            ]
+            ]
+        ])
+    ?>
 <?php
 	include('../includes/footer.html');
 ?>
